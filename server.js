c4b6bf4fef72f6f5e7f9c788dbb9e3e3c594d13e
saurabhsigma <saurabh962003@gmail.com>
2024-02-09 13:53:11 +0530

    second commit

--- server.js
+++ server.js
@@ -1,75 +1,22 @@
-const express = require("express")
-const app = express()
+// console.log("hello world")
 
+const express = require("express");
+const  app = express();
 const port = 3000;
 
 app.use(express.json())
-// function square(n){
-//     return n*n;
-// }
-const users = [{
-    name : "John",
-    kidneys : [{
-        healthy : false
-    }]
-}];
 
-app.get("/", (req,res)=>{
-    // res.send("hi therere");
-    // const n = req.headers.password;
-    // const ans = square(n);
-    // res.send("your ans is: " + ans);
-    const johnkidneys = users[0].kidneys;
-    const numberOfKidneys = johnkidneys.length;
-    let numberOfHealthyKidneys = 0;
+function cube(n){
+    return n*n*n;
+}
 
-    for(let i = 0;i<numberOfKidneys;i++){
-        if(johnkidneys[i].healthy){
-            numberOfHealthyKidneys = numberOfHealthyKidneys +1;
-        }
-    }
-    let numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
-
+app.get("/",(req,res)=>{
+    // res.send("hi there")
+    const n = req.query.n;
+    const ans = cube(n);
     res.json({
-        numberOfKidneys,numberOfHealthyKidneys,numberOfUnhealthyKidneys
-    })
-
+        ans
+    });
 })
 
-app.post("/", (req,res) =>{
-    const isHealthy = req.body.isHealthy;
-    users[0].kidneys.push({
-        healthy : isHealthy
-    })
-
-    res.json({
-        msg : "done!"
-    })
-})
-
-app.put("/", function(req,res){
-    for(let i=0;i<users[0].kidneys.length;i++){
-        users[0].kidneys[i].healthy=true;
-    }
-
-    res.json({
-        msg : "done"
-    })
-
-})
-
-app.delete("/", function(req,res){
-    const newKid = [];
-    for(let i =0; i<users[0].kidneys.length;i++){
-        if(users[0].kidneys[i].healthy){
-            newKid.push({healthy : true})
-        }
-    }
-    users[0].kidneys = newKid
-    res.json({
-        msg : "done"
-    })
-})
-
-
-app.listen(port);
+app.listen(port)
